68c30561a844e679e63a1c912d3ce446663a87a3
Vladimír Ondruška <Ondruska.vladimir@gmail.com>
2020-10-05 18:45:34 +0200

    Coded update functionality for my contacts app in Controller.java

--- src/com/ondruska/contacts/Controller.java
+++ src/com/ondruska/contacts/Controller.java
@@ -4,6 +4,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
@@ -49,10 +50,43 @@ public class Controller {
 
     @FXML
     public void editContactHandle(ActionEvent actionEvent) {
+        Contact selectedContact = myTable.getSelectionModel().getSelectedItem();
+        if(selectedContact == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("No contact selected");
+            alert.setHeaderText("You did not choose any contact to update");
+            alert.showAndWait();
+            return;
+        }
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainBorderPane.getScene().getWindow());
+        dialog.setTitle("Edit contact");
+        dialog.setHeaderText("Use this windows to edit contact.");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("dialogWindow.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+        }
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        DialogController dialogController = fxmlLoader.getController();
+        dialogController.fillEditForm(selectedContact);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+            dialogController.updateContact(selectedContact);
+        }
+
     }
 
     @FXML
     public void deleteContactHandle(ActionEvent actionEvent) {
+
     }
 
     @FXML
